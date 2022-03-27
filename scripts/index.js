ccdab3f6c6dd52649f0c50283aa263d75a7da59b
Dean Nash <shibta007@gmail.com>
2022-03-27 13:03:13 +0300

    V1.1

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,34 @@ const domElements = {
     editButton: document.querySelector(".dash__button_type_edit-info"),
     userTitle: document.querySelector(".dash__user-title"),
     userSubtitle: document.querySelector(".dash__user-subtitle"),
-    form: document.querySelector(".dash__form"),
-    nameInput: document.querySelector(".form__input-name"),
-    subtitleInput: document.querySelector(".form__input-subtitle"),
-    saveButton: document.querySelector(".form__button_type_save"),
-    closeButton: document.querySelector(".form__button_type_close"),
+    form: document.querySelector(".overlay__dash-form"),
+    nameInput: document.querySelector(".form__input_type_name"),
+    subtitleInput: document.querySelector(".form__input_type_subtitle"),
+    closeButton: document.querySelector(".overlay__button_type_close"),
     overlay: document.querySelector(".overlay"),
     likeButtons: document.querySelectorAll(".location__like-button")
 }
 
-domElements.likeButtons.forEach(button => {
+/** 
+    domElements.likeButtons.forEach(button => {
     button.addEventListener("click", ()=>button.classList.toggle("location__like-button_type_active"))
-});
-
-function toggleForm(){
-    const {form, overlay} = domElements;
-    overlay.classList.toggle("overlay__opened");
-    form.classList.toggle("form__opened");
-
-}
+}); 
+**/
 
 domElements.editButton.addEventListener("click", ()=>{
-    const {userTitle, userSubtitle, nameInput, subtitleInput} = domElements;
+    const {userTitle, userSubtitle, nameInput, subtitleInput, overlay} = domElements;
     nameInput.value = userTitle.textContent;
     subtitleInput.value = userSubtitle.textContent;
-    toggleForm();
+    overlay.classList.toggle("overlay__opened");
 })
 domElements.closeButton.addEventListener("click", (e)=>{
-    e.preventDefault();
-    toggleForm();
+    domElements.overlay.classList.toggle("overlay__opened");
 })
-domElements.saveButton.addEventListener("click", (e)=>{
+domElements.form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    const {userTitle, userSubtitle, nameInput, subtitleInput} = domElements;
+    const {userTitle, userSubtitle, nameInput, subtitleInput, overlay} = domElements;
     userTitle.textContent = nameInput.value
     userSubtitle.textContent = subtitleInput.value
-    toggleForm()
+    overlay.classList.toggle("overlay__opened");
 
 })
