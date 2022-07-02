a08a85909508fea27fff86896e6903c54cdd0ccb
Dean Nash <shibta007@gmail.com>
2022-07-02 11:42:45 +0300

    Revision 4

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,7 +3,6 @@ import Popup from "./Popup";
 export default class PopupWithImage extends Popup {
     constructor({popupSelector, containerSelector, imageSelector, textSelector}){
         super(popupSelector, containerSelector);
-        this._imageContainer = document.querySelector(containerSelector);
         this._imageElement = document.querySelector(imageSelector);
         this._textElement = document.querySelector(textSelector)
     }
